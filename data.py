d835f75a20fe1a71c6d45cb880d3a60eb5478469
HzahcyK <zhy547155267@qq.com>
2020-06-01 10:42:00 +0800

    0601-2

--- data.py
+++ data.py
@@ -34,20 +34,21 @@ class Database():
             self.cursor.close()
             self.conn.close()
         return result
-def caibian_read_count(start_time, end_time):
+def read_count(dp, start_time, end_time):
     db = Database(host="10.10.10.240", port=5432, user="root", password="tF!e5UN?iGMRkB7Z80Ln#O@uCsP^mS", db="dj_analytics")
     params = []
     start_time = start_time
     end_time = end_time
-    sql1 = "select sum(read_count) from views_article where editor like "%采编部%" and (pub_date between %s and %s);" %(start_time, end_time)
+    department = dp
+    sql = "select sum(read_count) from views_article where editor like'%%s%%' and (pub_date between %s and %s);" % (department, start_time, end_time)
     params.append(start_time)
     params.append(end_time)
-    r1 = db.get_all(sql1)
-    return r1
+    r = db.get_all(sql)
+    return r
 
 def main():
     result = caibian_read_count('2020-01-01 00:00:00', '2020-01-10 00:00:00')
-    return result
+    print(result)
 if __name__ == '__main__':
     main()
 
