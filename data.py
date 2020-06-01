838f3d0ca7a7c15fa3293b58f1af42a96dc36b7b
HzahcyK <zhy547155267@qq.com>
2020-06-01 13:03:10 +0800

    0601-11

--- data.py
+++ data.py
@@ -34,22 +34,22 @@ class Database():
             self.cursor.close()
             self.conn.close()
         return result
-def tai_read_count(start_time, end_time, dp):
+def tai_read_count(start_time, end_time, item):
     db = Database(host="10.10.10.240", port=5432, user="root", password="tF!e5UN?iGMRkB7Z80Ln#O@uCsP^mS", db="dj_analytics")
     params = []
     start_time = start_time
     end_time = end_time
-    dp = dp
+    dp = item
     sql = "select sum(read_count) from views_article where editor like'%%%s%%' and (pub_date between '%s' and '%s');" % (dp, start_time, end_time)
     params.append(start_time)
     params.append(end_time)
     r = db.get_all(sql)
     return r
-def county_read_count(start_time, end_time, county):
+def county_read_count(start_time, end_time, item):
     db = Database(host="10.10.10.240", port=5432, user="root", password="tF!e5UN?iGMRkB7Z80Ln#O@uCsP^mS", db="dj_analytics")
     start_time = start_time
     end_time = end_time
-    county = county
+    county = item
     sql = "select sum(read_count) from views_article where region like '%%%s%%' and (pub_date between '%s' and '%s');" % (county, start_time, end_time)
     r = db.get_all(sql)
     return r
@@ -58,27 +58,29 @@ def app_read_count(start_time, end_time, item):
     db = Database(host="10.10.10.240", port=5432, user="root", password="tF!e5UN?iGMRkB7Z80Ln#O@uCsP^mS", db="dj_analytics")
     start_time = start_time
     end_time = end_time
-    item = item
-    sql = "select sum(read_count) from views_article where category='%s' and (pub_date between '%s' and '%s');" % (item, start_time, end_time)
+    app_item = item
+    sql = "select sum(read_count) from views_article where category='%s' and (pub_date between '%s' and '%s');" % (app_item, start_time, end_time)
     r = db.get_all(sql)
     return r
 
-def read_count(part_tag, start_time, end_time, *args):
+def read_count(part_tag, start_time, end_time, item):
     db = Database(host="10.10.10.240", port=5432, user="root", password="tF!e5UN?iGMRkB7Z80Ln#O@uCsP^mS", db="dj_analytics")
     start_time = start_time
     end_time = end_time
     part_tag = part_tag
     switcher = {
-        1: tai_read_count(start_time, end_time, dp=args),
-        2: county_read_count(start_time, end_time, county=args),
-        3: app_read_count(start_time, end_time, itme=args),
+        1: tai_read_count,
+        2: county_read_count,
+        3: app_read_count,
     }
-    result = switcher.get(part_tag)()
+    method = switcher.get(part_tag)
+    if method:
+        method(start_time, end_time, item)
 
 def main():
     part_tag = input("请输入标签：")
-    demo = input("请输入模块：")
-    result = read_count(part_tag, '2020-01-01 00:00:00', '2020-01-10 00:00:00', demo)
+    item = input("请输入模块：")
+    result = read_count(part_tag, '2020-01-01 00:00:00', '2020-01-10 00:00:00',item)
     print(result)
 if __name__ == '__main__':
     main()
